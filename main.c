d132481dfecb99ec14d731ab3a431cc85c9aa703
jwdlplus <jwqueen04@gmail.com>
2025-11-20 15:28:00 +0900

    12 실습2

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "board.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +15,16 @@ int main(int argc, char *argv[])
   printf("=================================================\n");
   printf("=================================================\n\n");
   
+  //step 1. initialization (player name setting, variables)
+  
+  //step 2. turn play (do-while)
+  // 2-1. status printing
+  board_printBoardStatus();
+  // 2-2. roll die
+  // 2-3. move (result)
+  // 2-4. change turn, shark move
+  
+  //step3. game end(winner printing)
   
   //ending
   printf("\n\n\n\n\n\n\n");
